c220530bc500a6a08b01f48abdeb0707d58177da
Victor Mal <VictorianusVelikyu@gmail.com>
2021-09-28 00:28:28 +0300

    Add style to BurgerMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
+import BurgerMenu from "./components/BurgerMenu";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-
+    console.log();
   }
   render() {
     return(
-      
+      <BurgerMenu />
     )
   }
 }
